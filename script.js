b58007d11ff3f5f98d2ba569a63e2e4ae4044a81
mirela <sousa_mirela07@icloud.com>
2025-04-02 18:23:14 -0300

    projeto finalizado

---
+++ script.js
@@ -0,0 +1,10 @@
+function paraMudar() {
+    const HTML = document.documentElement
+
+  //  if(html.classList.contains('light')){
+   //      html.classList.remove('light')
+  //  }else{
+   //     html.classList.add('light')}
+
+   HTML.classList.toggle("light")
+}
